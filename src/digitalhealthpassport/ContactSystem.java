aa37e2ed5011694a9bc1619db3ab829bf83d6b06
kris <kris@192.168.0.107>
2021-04-22 14:49:23 -0400

    changed variable name to physNum & datatype to int

--- src/digitalhealthpassport/ContactSystem.java
+++ src/digitalhealthpassport/ContactSystem.java
@@ -4,7 +4,7 @@ public class ContactSystem {
 
     private String physicianName;
     private String physicianHospital;
-    private String physicianNumber;
+    private int physNumber;
     private String patientName;
     private String patienDOB;
     private String healthCard; //maybe ENUM if the government has all the health card number information. 
