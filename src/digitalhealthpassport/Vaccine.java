74770065e06b9c82dfca295a3b370ff02d74ac31
kris <kris@192.168.0.107>
2021-04-22 16:28:23 -0400

    changes covidVaccine class: - combined repeating elements to Vaccine class - created covid19 vaccines as an object - added another vaccine as an object

--- src/digitalhealthpassport/Vaccine.java
+++ src/digitalhealthpassport/Vaccine.java
@@ -5,6 +5,12 @@ public class Vaccine {
     private String vaccineName; 
     private String vaccineLotNumber; 
     private String manufacturedDate;
-    private String notes;
-
+    private String manufacturer;
+    private String dateAdministered;
+    private String detail;
+    private Person person;
+    private Practitioner practitioner;
+    
+    Vaccine covid19 = new Vaccine();
+    Vaccine fluShot2021 = new Vaccine();
 }
